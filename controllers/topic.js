e9943f8f5407d4ca39ee82d5760fbce283a24289
heolife <heolife@126.com>
2017-11-16 17:29:59 +0800

    test page added. layout tab testing ongoing

--- controllers/topic.js
+++ controllers/topic.js
@@ -18,7 +18,7 @@ var store        = require('../common/store');
 var config       = require('../config');
 var _            = require('lodash');
 var cache        = require('../common/cache');
-var logger = require('../common/logger')
+var logger       = require('../common/logger')
 
 /**
  * Topic page
